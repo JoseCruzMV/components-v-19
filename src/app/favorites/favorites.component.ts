43c1edbc99d15a6643d08ed5dcff6864ecb00260
josecruz <josecruzmv@outlook.com>
2026-06-03 12:34:42 -0600

    Add FavoritesComponent and integrate it into ProductListComponent

---
+++ src/app/favorites/favorites.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+import { ProductsService } from '../products.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-favorites',
+  imports: [CommonModule],
+  templateUrl: './favorites.component.html',
+  styleUrl: './favorites.component.css',
+})
+export class FavoritesComponent implements OnInit {
+  products: Product[] = [];
+
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.products = this.productsService.getProducts();
+  }
+}
