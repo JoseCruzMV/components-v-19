4c2443699f5b9c9de4bb5b431395078c2d8972eb
josecruz <josecruzmv@outlook.com>
2026-06-03 14:17:58 -0600

    Add favoritesFactory and update FavoritesComponent to use it for ProductsService

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../product';
 import { ProductsService } from '../products.service';
-import { FavoritesService } from '../favorites.service';
+import { favoritesFactory } from '../favorites';
 
 @Component({
   selector: 'app-favorites',
   imports: [],
   templateUrl: './favorites.component.html',
   styleUrl: './favorites.component.css',
-  providers: [{ provide: ProductsService, useClass: FavoritesService }],
+  providers: [{ provide: ProductsService, useFactory: favoritesFactory(true) }],
 })
 export class FavoritesComponent implements OnInit {
   products: Product[] = [];
