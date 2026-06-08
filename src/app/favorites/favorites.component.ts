10fcaed63c1be41ab8d69d64ded10b5603114a7b
josecruz <josecruzmv@outlook.com>
2026-06-08 12:31:33 -0600

    Refactor FavoritesComponent and ProductViewService to use async pipe and manage subscriptions with DestroyRef

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -2,22 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../product';
 import { ProductsService } from '../products.service';
 import { favoritesFactory } from '../favorites';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-favorites',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './favorites.component.html',
   styleUrl: './favorites.component.css',
   providers: [{ provide: ProductsService, useFactory: favoritesFactory(true) }],
 })
 export class FavoritesComponent implements OnInit {
-  products: Product[] = [];
+  products$: Observable<Product[]> | undefined;
 
   constructor(private productsService: ProductsService) {}
 
   ngOnInit(): void {
-    this.productsService.getProducts().subscribe((products) => {
-      this.products = products;
-    });
+    this.products$ = this.productsService.getProducts();
   }
 }
--- src/app/product-view/product-view.service.ts
+++ src/app/product-view/product-view.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { Injectable, DestroyRef, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ProductsService } from '../products.service';
 import { Product } from '../product';
 
@@ -6,11 +7,15 @@ import { Product } from '../product';
 export class ProductViewService {
   private product: Product | undefined;
   products: Product[] = [];
+  private destroyRef = inject(DestroyRef);
 
   constructor(private productsService: ProductsService) {
-    this.productsService.getProducts().subscribe((products) => {
-      this.products = products;
-    });
+    this.productsService
+      .getProducts()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((products) => {
+        this.products = products;
+      });
   }
 
   getProducts(id: number): Product | undefined {
