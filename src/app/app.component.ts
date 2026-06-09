60ed21dbccb40df597df51ee9cad480bbfa96620
josecruz <josecruzmv@outlook.com>
2026-06-09 13:06:44 -0600

    Add authentication functionality; implement AuthService and AuthComponent for user login/logout, update ProductDetailComponent to conditionally display "Add to cart" button based on authentication status.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,16 @@ import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { CopyrightDirective } from './copyright.directive';
 import { APP_SETTINGS } from './app.settings';
+import { AuthComponent } from './auth/auth.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductListComponent, CopyrightDirective],
+  imports: [
+    RouterOutlet,
+    ProductListComponent,
+    CopyrightDirective,
+    AuthComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -3,6 +3,7 @@ import { CommonModule, AsyncPipe } from '@angular/common';
 import { Product } from '../product';
 import { Observable } from 'rxjs';
 import { ProductsService } from '../products.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -16,7 +17,10 @@ export class ProductDetailComponent implements OnChanges {
   added = output();
   deleted = output();
 
-  constructor(private productService: ProductsService) {}
+  constructor(
+    private productService: ProductsService,
+    public authService: AuthService,
+  ) {}
 
   ngOnChanges(): void {
     this.product$ = this.productService.getProduct(this.id()!);
