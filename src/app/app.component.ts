da51ca694c04de4e35a41d4d1cfc48adf96a6742
josecruz <josecruzmv@outlook.com>
2026-06-09 10:02:17 -0600

    Refactor AppComponent to use settings for title management; update app configuration to include API URL and clean up imports in ProductsService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,15 @@
-import { Component, inject, Signal, computed, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { CopyrightDirective } from './copyright.directive';
-import { APP_SETTINGS, appSettings } from './app.settings';
-import { Observable, timestamp } from 'rxjs';
+import { APP_SETTINGS } from './app.settings';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, ProductListComponent, CopyrightDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  providers: [{ provide: APP_SETTINGS, useValue: appSettings }],
 })
 export class AppComponent {
   settings = inject(APP_SETTINGS);
-  title: Signal<string> = signal('');
-  currentDate = signal(new Date());
-
-  title$ = new Observable<void>((observer) => {
-    setInterval(() => {
-      observer.next();
-    }, 2000);
-  });
-
-  constructor() {
-    this.title$.subscribe(this.setTitle);
-    this.title = computed(() => {
-      return `${this.settings.title} ${this.currentDate()}`;
-    });
-  }
-
-  private setTitle = () => {
-    this.currentDate.set(new Date());
-  };
 }
--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -1,47 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { Product } from './product';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { APP_SETTINGS } from './app.settings';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
-  private products: Product[] = [
-    {
-      id: 1,
-      title: 'Keyboard',
-      price: 100,
-      categories: {
-        1: 'Computing',
-        2: 'Peripherals',
-      },
-    },
-    {
-      id: 2,
-      title: 'Microphone',
-      price: 35,
-      categories: { 3: 'Multimedia' },
-    },
-    {
-      id: 3,
-      title: 'Web camera',
-      price: 79,
-      categories: {
-        1: 'Computing',
-        3: 'Multimedia',
-      },
-    },
-    {
-      id: 4,
-      title: 'Tablet',
-      price: 500,
-      categories: { 4: 'Entertainment' },
-    },
-  ];
+  private productsUrl = inject(APP_SETTINGS).apiUrl + '/products';
 
-  constructor() {}
+  private products: Product[] = [];
+
+  constructor(private http: HttpClient) {}
 
   getProducts(): Observable<Product[]> {
-    return of(this.products);
+    return this.http.get<Product[]>(this.productsUrl);
   }
 }
