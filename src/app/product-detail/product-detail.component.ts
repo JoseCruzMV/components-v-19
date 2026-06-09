51da758aab021d069a871224584aec84074d5764
josecruz <josecruzmv@outlook.com>
2026-06-09 10:38:02 -0600

    Refactor ProductDetailComponent to use async pipe for product data; update ProductsService to fetch product by ID; modify ProductListComponent to pass product ID instead of the entire product object.

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,17 +1,25 @@
-import { Component, input, output } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, input, output, OnChanges } from '@angular/core';
+import { CommonModule, AsyncPipe } from '@angular/common';
 import { Product } from '../product';
+import { Observable } from 'rxjs';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [CommonModule],
+  imports: [CommonModule, AsyncPipe],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css',
 })
-export class ProductDetailComponent {
-  product = input<Product>();
-
+export class ProductDetailComponent implements OnChanges {
+  id = input<number>();
+  product$: Observable<Product> | undefined;
   added = output();
+
+  constructor(private productService: ProductsService) {}
+
+  ngOnChanges(): void {
+    this.product$ = this.productService.getProduct(this.id()!);
+  }
 
   addToCart() {
     this.added.emit();
