84a867082aa1751aa404953255b4cbe3b41c4af1
josecruz <josecruzmv@outlook.com>
2026-06-10 10:48:19 -0600

    Add CartComponent and update routing; enhance app layout with navigation links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,12 @@
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ProductListComponent } from './product-list/product-list.component';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { CopyrightDirective } from './copyright.directive';
 import { APP_SETTINGS } from './app.settings';
 import { AuthComponent } from './auth/auth.component';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    ProductListComponent,
-    CopyrightDirective,
-    AuthComponent,
-  ],
+  imports: [RouterOutlet, RouterLink, CopyrightDirective, AuthComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'products',
+    component: ProductListComponent,
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
+  },
+];
