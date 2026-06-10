9c757dcfa8ab7772bcbacb006ce767d462e45356
josecruz <josecruzmv@outlook.com>
2026-06-10 10:28:56 -0600

    Remove authInterceptor and related tests; update appConfig to use default HttpClient provider.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,14 +3,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { APP_SETTINGS, appSettings } from './app.settings';
-import { withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(),
     { provide: APP_SETTINGS, useValue: appSettings },
   ],
 };
