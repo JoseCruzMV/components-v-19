33b3dea924e7ecbd6c1177d0100a998d3de775a0
josecruz <josecruzmv@outlook.com>
2026-06-17 10:40:08 -0600

    Refactor product creation component to simplify form handling; add validation messages for title and price inputs

--- src/app/product-create/product-create.component.ts
+++ src/app/product-create/product-create.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ProductsService } from '../products.service';
 import { Router } from '@angular/router';
 import {
   FormControl,
   FormGroup,
   ReactiveFormsModule,
-  FormBuilder,
+  Validators,
 } from '@angular/forms';
 
 @Component({
@@ -14,36 +14,29 @@ import {
   templateUrl: './product-create.component.html',
   styleUrl: './product-create.component.css',
 })
-export class ProductCreateComponent implements OnInit {
-  productForm:
-    | FormGroup<{
-        title: FormControl<string>;
-        price: FormControl<number | undefined>;
-        category: FormControl<string>;
-      }>
-    | undefined;
+export class ProductCreateComponent {
+  productForm = new FormGroup({
+    title: new FormControl('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    price: new FormControl<number | undefined>(undefined, {
+      nonNullable: true,
+      validators: [Validators.required, Validators.min(1)],
+    }),
+    category: new FormControl('', {
+      nonNullable: true,
+    }),
+  });
 
   constructor(
     private productService: ProductsService,
     private router: Router,
-    private builder: FormBuilder,
   ) {}
 
   createProduct() {
-    this.productService.addProduct(this.productForm!.value).subscribe(() => {
+    this.productService.addProduct(this.productForm.value).subscribe(() => {
       this.router.navigate(['/products']);
     });
-  }
-
-  buildForm() {
-    this.productForm = this.builder.nonNullable.group({
-      title: [''],
-      price: this.builder.nonNullable.control<number | undefined>(undefined),
-      category: [''],
-    });
-  }
-
-  ngOnInit(): void {
-    this.buildForm();
   }
 }
