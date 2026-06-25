953d901c4e7143c351131aebc69956e11842ad48
josecruz <josecruzmv@outlook.com>
2026-06-25 13:09:41 -0600

    Implement OnInit lifecycle hook in ProductCreateComponent; reset price field on category change

--- src/app/product-create/product-create.component.ts
+++ src/app/product-create/product-create.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, DestroyRef } from '@angular/core';
 import { ProductsService } from '../products.service';
 import { Router } from '@angular/router';
 import {
@@ -8,6 +8,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { priceMaximumValidator } from '../price-maximum.validator';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-product-create',
@@ -15,7 +16,8 @@ import { priceMaximumValidator } from '../price-maximum.validator';
   templateUrl: './product-create.component.html',
   styleUrl: './product-create.component.css',
 })
-export class ProductCreateComponent {
+export class ProductCreateComponent implements OnInit {
+  private distroyRef = inject(DestroyRef);
   productForm = new FormGroup({
     title: new FormControl('', {
       nonNullable: true,
@@ -38,6 +40,14 @@ export class ProductCreateComponent {
     private productService: ProductsService,
     private router: Router,
   ) {}
+
+  ngOnInit(): void {
+    this.productForm.controls.category.valueChanges
+      .pipe(takeUntilDestroyed(this.distroyRef))
+      .subscribe(() => {
+        this.productForm.controls.price.reset();
+      });
+  }
 
   createProduct() {
     this.productService.addProduct(this.productForm.value).subscribe(() => {
